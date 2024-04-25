28b553d194a51ba6a95b24c9a53b38807a76323e
riru12 <brilljulianrina@gmail.com>
2024-04-25 15:36:49 +0800

    adjusted hero section animation

--- app/components/hero.jsx
+++ app/components/hero.jsx
@@ -4,22 +4,22 @@ import "../assets/css/hero.css";
 const Hero = () => {
     return(
         <div id="hero" className="flex flex-col items-center mt-36 mx-0 px-4 md:mx-[20vw]">
-            <div className="unfancy">Hey! My name is</div>
+            <div className="unfancy">HEY! MY NAME IS</div>
             <div className="flex justify-center items-center h-52 w-full font-bold fancy name">
                 <div className="font-bold enhance">
-                    <span className="letter shrink">B</span>
-                    <span className="special grow">r</span>
-                    <span className="special grow">i</span>
-                    <span className="letter shrink">l</span>
-                    <span className="letter shrink">l</span>
-                    <span>&nbsp;</span>
-                    <span className="letter shrink">J</span>
-                    <span className="special text-[0px] grow">r</span>
-                    <span className="special grow">u</span>
-                    <span className="letter shrink">l</span>
-                    <span className="letter shrink">i</span>
-                    <span className="letter shrink">a</span>
-                    <span className="letter shrink">n</span>
+                    <span className="letter">B</span>
+                    <span className="special">r</span>
+                    <span className="special">i</span>
+                    <span className="letter">l</span>
+                    <span className="letter">l</span>
+                    <span className="letter">&nbsp;</span>
+                    <span className="letter">J</span>
+                    <span className="special text-[0px]">r</span>
+                    <span className="special">u</span>
+                    <span className="letter">l</span>
+                    <span className="letter">i</span>
+                    <span className="letter">a</span>
+                    <span className="letter">n</span>
                 </div>
             </div>
         </div>
