a7286c2277c99a5434e816af2b4ff99102ec5bd5
riru12 <brilljulianrina@gmail.com>
2024-05-01 03:12:09 +0800

    removed warnings, added hover effect to projects

--- app/components/projects.jsx
+++ app/components/projects.jsx
@@ -13,19 +13,21 @@ const Projects = () => {
                 </div>
                 <div className="grid auto-rows-fr gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
                     {projects.map(project => (
-                        <div key={project.id} className="flex flex-col bg-neutral-900 rounded p-6 gap-10 justify-between">
-                            <div className="flex flex-row justify-between">
-                                <FolderOutline strokeColor="#bcedc5" />
-                                <div className="flex gap-3">
-                                    {project.gitlink && <a href={project.gitlink}><GitLink fillColor="#d4d4d4" /></a>}
-                                    {project.exterlink && <a href={project.exterlink}><ExterLink strokeColor="#d4d4d4" /></a>}
+                        <div key={project.id} className="group">
+                            <div className="flex flex-col bg-neutral-900 h-full rounded p-6 gap-10 justify-between group-hover:translate-y-[-10px] transition-transform">
+                                <div className="flex flex-row justify-between">
+                                    <FolderOutline strokeColor="#bcedc5" />
+                                    <div className="flex gap-3">
+                                        {project.gitlink && <a href={project.gitlink}><GitLink fillColor="#d4d4d4" /></a>}
+                                        {project.exterlink && <a href={project.exterlink}><ExterLink strokeColor="#d4d4d4" /></a>}
+                                    </div>
                                 </div>
-                            </div>
-                            <div>
-                                <div id="projectName" className="font-bold text-neutral-300">{project.name}</div>
-                                <div className="flex flex-col gap-4">
-                                    <div id="projectDesc" className="text-neutral-400">{project.description}</div>
-                                    <div id="projectTech" className="font-mono text-neutral-500 text-sm">{project.technologies.join(' ')}</div>
+                                <div>
+                                    <div id="projectName" className="font-bold text-neutral-300 group-hover:text-[#bcedc5] transition-colors">{project.name}</div>
+                                    <div className="flex flex-col gap-4">
+                                        <div id="projectDesc" className="text-neutral-400">{project.description}</div>
+                                        <div id="projectTech" className="font-mono text-neutral-500 text-sm">{project.technologies.join(' ')}</div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
