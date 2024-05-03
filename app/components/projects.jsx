0a658b122e8937315352dabdb35328abe67a53f5
riru12 <brilljulianrina@gmail.com>
2024-05-03 16:46:08 +0800

    updated formatting of project tabs

--- app/components/projects.jsx
+++ app/components/projects.jsx
@@ -16,42 +16,42 @@ const Projects = () => {
                 <div className="grid auto-rows-fr gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
                     {projects.map(project => (
                         <div key={project.id} className="group cursor-default">
-                            <div className="flex flex-col bg-neutral-900 h-full rounded p-6 gap-10 justify-between sm:group-hover:translate-y-[-10px] transition-transform">
-                                <div className="flex flex-row justify-between">
-                                    <FolderOutline strokeColor="#bcedc5" />
-                                    <div className="flex gap-3 h-min">
-                                        {project.doclink && 
-                                            <div className="relative group/git h-min">
-                                                <a href={project.doclink} target="_blank"><DocumentIcon strokeColor="#d4d4d4" /></a>
-                                                <div className="absolute whitespace-nowrap text-xs bg-neutral-800 px-3 py-2 rounded-xl font-mono transition-all left-[50%] translate-x-[-50%] top-0 translate-y-[-110%] hidden group-hover/git:block">Documentation</div>
-                                            </div>
-                                        }
-                                        {project.figmalink && 
-                                            <div className="relative group/git h-min">
-                                                <a href={project.figmalink} target="_blank"><FigmaIcon strokeColor="#d4d4d4"/></a>
-                                                <div className="absolute whitespace-nowrap text-xs bg-neutral-800 px-3 py-2 rounded-xl font-mono transition-all left-[50%] translate-x-[-50%] top-0 translate-y-[-110%] hidden group-hover/git:block">Figma</div>
-                                            </div>
-                                        }
-                                        {project.gitlink && 
-                                            <div className="relative group/git h-min">
-                                                <a href={project.gitlink} target="_blank"><GithubIcon fillColor="#d4d4d4" /></a>
-                                                <div className="absolute whitespace-nowrap text-xs bg-neutral-800 px-3 py-2 rounded-xl font-mono transition-all left-[50%] translate-x-[-50%] top-0 translate-y-[-110%] hidden group-hover/git:block">Git Repo</div>
-                                            </div>
-                                        }
-                                        {project.exterlink && 
-                                            <div className="relative group/live h-min">
-                                                <a href={project.exterlink} target="_blank"><ExterLink strokeColor="#d4d4d4" /></a>
-                                                <div className="absolute whitespace-nowrap text-xs bg-neutral-800 px-3 py-2 rounded-xl font-mono transition-all left-[50%] translate-x-[-50%] top-0 translate-y-[-110%] hidden group-hover/live:block">Live Link</div>
-                                            </div>
-                                        }
+                            <div className="grid grid-cols-1 bg-neutral-900 h-full rounded p-6 sm:group-hover:translate-y-[-10px] transition-transform">
+                                <div>
+                                    <div className="flex flex-row justify-between">
+                                        <FolderOutline strokeColor="#bcedc5" />
+                                        <div className="flex gap-3 h-min">
+                                            {project.doclink && 
+                                                <div className="relative group/git h-min">
+                                                    <a href={project.doclink} target="_blank"><DocumentIcon strokeColor="#d4d4d4" /></a>
+                                                    <div className="absolute whitespace-nowrap text-xs bg-neutral-800 px-3 py-2 rounded-xl font-mono transition-all left-[50%] translate-x-[-50%] top-0 translate-y-[-110%] hidden group-hover/git:block">Documentation</div>
+                                                </div>
+                                            }
+                                            {project.figmalink && 
+                                                <div className="relative group/git h-min">
+                                                    <a href={project.figmalink} target="_blank"><FigmaIcon strokeColor="#d4d4d4"/></a>
+                                                    <div className="absolute whitespace-nowrap text-xs bg-neutral-800 px-3 py-2 rounded-xl font-mono transition-all left-[50%] translate-x-[-50%] top-0 translate-y-[-110%] hidden group-hover/git:block">Figma</div>
+                                                </div>
+                                            }
+                                            {project.gitlink && 
+                                                <div className="relative group/git h-min">
+                                                    <a href={project.gitlink} target="_blank"><GithubIcon fillColor="#d4d4d4" /></a>
+                                                    <div className="absolute whitespace-nowrap text-xs bg-neutral-800 px-3 py-2 rounded-xl font-mono transition-all left-[50%] translate-x-[-50%] top-0 translate-y-[-110%] hidden group-hover/git:block">Git Repo</div>
+                                                </div>
+                                            }
+                                            {project.exterlink && 
+                                                <div className="relative group/live h-min">
+                                                    <a href={project.exterlink} target="_blank"><ExterLink strokeColor="#d4d4d4" /></a>
+                                                    <div className="absolute whitespace-nowrap text-xs bg-neutral-800 px-3 py-2 rounded-xl font-mono transition-all left-[50%] translate-x-[-50%] top-0 translate-y-[-110%] hidden group-hover/live:block">Live Link</div>
+                                                </div>
+                                            }
+                                        </div>
                                     </div>
+                                    <div id="projectName" className="font-bold text-neutral-300 group-hover:text-[#bcedc5] transition-colors mt-10">{project.name}</div>
+                                    <div id="projectDesc" className="text-neutral-400 mt-2 mb-6">{project.description}</div>
                                 </div>
                                 <div>
-                                    <div id="projectName" className="font-bold text-neutral-300 group-hover:text-[#bcedc5] transition-colors">{project.name}</div>
-                                    <div className="flex flex-col gap-4">
-                                        <div id="projectDesc" className="text-neutral-400">{project.description}</div>
-                                        <div id="projectTech" className="font-mono text-neutral-500 text-xs">{project.technologies.join(' ')}</div>
-                                    </div>
+                                    <div id="projectTech" className="flex font-mono text-neutral-500 text-xs h-full items-end">{project.technologies.join(' ')}</div>
                                 </div>
                             </div>
                         </div>
